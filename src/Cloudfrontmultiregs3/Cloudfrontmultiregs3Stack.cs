8f949e07152314457e92916bce4bc577bf3848eb
brendonparker <brendon.parker@rfsmart.com>
2022-10-14 15:28:16 -0400

    Initial commit

---
+++ src/Cloudfrontmultiregs3/Cloudfrontmultiregs3Stack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Cloudfrontmultiregs3
+{
+    public class Cloudfrontmultiregs3Stack : Stack
+    {
+        internal Cloudfrontmultiregs3Stack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
